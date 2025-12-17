9a037c207c46b0207c198ad2123834f5393a076e
yadhu-tj <yadhunandhantj@gmail.com>
2025-12-17 23:17:45 +0530

    created a new routes file for the info pages for the art-bay project

---
+++ blueprints/info_routes.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template
+
+info_bp = Blueprint('info', __name__)
+
+@info_bp.route('/artists')
+def artists_showcase():
+    # In a real app, you'd fetch artists from the DB here.
+    # For now, we render the template.
+    return render_template('info/artists.html')
+
+@info_bp.route('/guide/artists')
+def artist_guide():
+    return render_template('info/guide_artists.html')
+
+@info_bp.route('/guide/customers')
+def customer_guide():
+    return render_template('info/guide_customers.html')
+
+@info_bp.route('/about')
+def about_us():
+    return render_template('info/about_us.html')
